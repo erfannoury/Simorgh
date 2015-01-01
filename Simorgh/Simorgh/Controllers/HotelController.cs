e21fd956518a034843f97280cd09a6b41544c214
Ehsan <ehsan.silverman@gmail.com>
2015-01-01 18:38:45 +0330

    Added MVC elements for Hotel and RoomType
    Model, View and Controller classes for entities Hotel and RoomType were
    added according to ERD.
    Migrations InitialCreate (for RoomType) and StrLen (for RoomType and
    Hotel) were created.

---
+++ Simorgh/Simorgh/Controllers/HotelController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Simorgh.Models;
+
+namespace Simorgh.Controllers
+{
+    public class HotelController : Controller
+    {
+        private HotelDBContext db = new HotelDBContext();
+
+        // GET: /Hotel/
+        public ActionResult Index()
+        {
+            return View(db.Hotels.ToList());
+        }
+
+        // GET: /Hotel/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Hotel hotel = db.Hotels.Find(id);
+            if (hotel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hotel);
+        }
+
+        // GET: /Hotel/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Hotel/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Id,ImageFolderId,Name,CityIdCity,Address,Longitude,Latitude,Description,Star")] Hotel hotel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Hotels.Add(hotel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(hotel);
+        }
+
+        // GET: /Hotel/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Hotel hotel = db.Hotels.Find(id);
+            if (hotel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hotel);
+        }
+
+        // POST: /Hotel/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,ImageFolderId,Name,CityIdCity,Address,Longitude,Latitude,Description,Star")] Hotel hotel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(hotel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(hotel);
+        }
+
+        // GET: /Hotel/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Hotel hotel = db.Hotels.Find(id);
+            if (hotel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hotel);
+        }
+
+        // POST: /Hotel/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Hotel hotel = db.Hotels.Find(id);
+            db.Hotels.Remove(hotel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Simorgh/Simorgh/Models/HotelModels.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace Simorgh.Models
+{
+    public class Hotel
+    {
+        
+        public int Id { get; set; }
+        public int ImageFolderId { get; set; }
+
+        [StringLength(60, ErrorMessage = "Name cannot be longer than 60 characters.")]
+        public string Name { get; set; }
+        
+        public int CityIdCity { get; set; }
+        
+        [StringLength(500, ErrorMessage = "Address cannot be longer than 500 characters.")]
+        public string Address { get; set; }
+
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        
+        public string Description { get; set; }
+
+        public int Star { get; set; }
+    }
+
+    public class HotelDBContext : DbContext
+    {
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
