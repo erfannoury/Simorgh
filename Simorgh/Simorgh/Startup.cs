3b04d4e307411c9139a8eb6b8a43750f1b9733e5
Erfan Noury <erfannoury@users.noreply.github.com>
2015-01-01 14:29:10 +0330

    Created ASP.NET MVC Project
    default setup contains almost everything we want.

---
+++ Simorgh/Simorgh/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Simorgh.Startup))]
+namespace Simorgh
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
