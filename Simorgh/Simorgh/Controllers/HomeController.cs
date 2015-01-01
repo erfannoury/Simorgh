54aa5704dbfd8cd83f7764e5770bf3bd128fbb35
Erfan Noury <erfannoury@users.noreply.github.com>
2015-01-01 17:46:21 +0330

    [Temp] - Turned off SSL temporarily
    For testing purposes, SSL is turned off for now

--- Simorgh/Simorgh/Controllers/HomeController.cs
+++ Simorgh/Simorgh/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace Simorgh.Controllers
 {
-    [RequireHttps]
+    //[RequireHttps]
     public class HomeController : Controller
     {
         public ActionResult Index()
