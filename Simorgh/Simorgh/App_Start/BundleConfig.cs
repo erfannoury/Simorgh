d200c2f5c96997b5ddcc88120a77a1002310acb7
Mohammad Ali BagherShemirani <bagherbal@gmail.com>
2015-01-29 15:20:52 +0330

    Remove phase 2 UI
    Remove all files of phase2's UI, because erfan girs too much.
    The new ui will be written from scratch base on Boostrap material.

--- Simorgh/Simorgh/App_Start/BundleConfig.cs
+++ Simorgh/Simorgh/App_Start/BundleConfig.cs
@@ -23,37 +23,10 @@ namespace Simorgh
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/main_scripts").Include(
-                      "~/Scripts/new/js/jquery.min.js",
-                      "~/Scripts/new/jquery-migrate-1.2.1.min.js",
-                      "~/Scripts/new/bootstrap.min.js",
-                      "~/Scripts/new/js/style-switcher/colorpicker.js",
-                      "~/Scripts/new/js/style-switcher/jquery.cookie.js",
-                      "~/Scripts/new/js/style-switcher/styleswitch.js",
-                      "~/Scripts/new/js/style-switcher/switcher2.js",
-                      "~/Scripts/new/js/jquery.customSelect.latest.min.js",
-                      "~/Scripts/new/js/bootstrap-datepicker.js",
-                      "~/Scripts/new/js/jquery-hoverIntent.js",
-                      "~/Scripts/new/js/jquery.flexslider.js",
-                      "~/Scripts/new/js/jquery.counterup.min.js",
-                      "~/Scripts/new/js/waypoints.min.js",
-                      "~/Scripts/new/js/jquery.prettyPhoto.js",
-                      "~/Scripts/new/js/custom.js"));
-
-//            bundles.Add(new StyleBundle("~/Content/css").Include(
-//                      "~/Content/bootstrap.css",
-//                      "~/Content/site.css"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/new/googlefont.css",
-                      "~/Content/new/bootstrap.min.css",
-                      "~/Content/new/css/green/style.css",
-                      "~/Content/new/css/datepicker.css",
-                      "~/Content/new/font-awesome.min.css",
-                      "~/Content/new/css/hover-min.css",
-                      "~/Content/new/css/flexslider.css",
-                      "~/Content/new/css/prettyPhoto.css",
-                      "~/Content/new/css/style-switcher.css"));
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
         
         }
     }
