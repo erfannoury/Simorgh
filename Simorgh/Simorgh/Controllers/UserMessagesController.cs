0f245f9e2185a989942dcacc1325c25162ebd631
Ehsan <ehsan.silverman@gmail.com>
2015-01-30 05:15:31 +0330

    Initial message service

---
+++ Simorgh/Simorgh/Controllers/UserMessagesController.cs
@@ -0,0 +1,91 @@
+﻿using Simorgh.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Simorgh.Controllers
+{
+    public class UserMessagesController : Controller
+    {
+        private MessageDbContext db = new MessageDbContext();
+
+        //
+        // GET: /UserMessages/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Inbox()
+        {
+            String username = User.Identity.Name;
+            var entries = from u in db.UserMessages where ((u.ToUserName == username) && (u.isRead == false)) select u;
+            return View(entries.ToList());
+        }
+
+        public ActionResult Sent()
+        {
+            String username = User.Identity.Name;
+            var entries = from u in db.UserMessages where (u.FromUserName == username) select u;
+            return View(entries.ToList());
+        }
+
+        // GET: /Inbox/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserMessage usermessage = db.UserMessages.Find(id);
+
+            if (usermessage == null)
+            {
+                return HttpNotFound();
+            }
+
+            db.UserMessages.Remove(usermessage);
+            usermessage.isRead = true;
+            //db.UserMessages.Add(usermessage);
+            db.SaveChanges();
+
+            return View(usermessage);
+        }
+
+        // GET: /Inbox/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Inbox/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FromUserName,ToUserName,ReplyToMessage,MessageText,MessageTime,isRead")] UserMessage usermessage)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserMessages.Add(usermessage);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(usermessage);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+	}
+}
---
+++ Simorgh/Simorgh/Models/UserMessageModels.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Simorgh.Models
+{
+    public class UserMessage
+    {
+        [Key, DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string FromUserName { get; set; }
+
+        [Required]
+        public string ToUserName { get; set; }
+
+        public int? ReplyToMessage { get; set; }
+
+        [Required]
+        public string MessageText { get; set; }
+
+        [Required]
+        public DateTime MessageTime { get; set; }
+
+        public Boolean isRead { get; set; }
+
+    }
+
+    public class MessageDbContext : DbContext
+    {
+        public DbSet<UserMessage> UserMessages { get; set; }
+    }
+}
